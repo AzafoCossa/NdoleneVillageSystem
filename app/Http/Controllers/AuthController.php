0ca84972769346301758ffc6bcd9dfd945885f65
Azafo Cossa <azafoalexandre@outlook.com>
2025-03-02 00:28:05 +0200

    Implement login and logout methods in AuthController with validation and error handling

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,5 +8,32 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-   
+    public function login(Request $request){
+
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ], [
+            'email.required' => 'O campo email é obrigatório',
+            'email.email' => 'O email informado é inválido',
+            'password.required' => 'O campo password é obrigatório'
+        ]);
+
+        if (!auth()->attempt($validated, $request->remember_me)) {
+            return redirect()->back()->withInput()->withErrors([
+                'email' => 'O email ou password estão incorretos',
+            ]);
+        }
+
+        return redirect()->route('dashboard');
+    }
+    public function logout(Request $request){
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('home');
+    }
 }
