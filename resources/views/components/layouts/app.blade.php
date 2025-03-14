535733f21a794de2c95a0842c9fb326f3c556093
Azafo Cossa <azafoalexandre@outlook.com>
2025-03-14 21:29:16 +0200

    Update layout template: add PayPal SDK script and adjust meta tags

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <title>{{ $title ?? "Page Title" }}</title>
 
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <script src="https://www.paypal.com/sdk/js?components=card-fields&client-id={{
+                env('PAYPAL_CLIENT_ID')
+            }}"></script>
     </head>
     <body>
         {{ $slot }}
