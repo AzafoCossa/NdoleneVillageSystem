c45c550fb9667f0e445789a955964f417c2f8c0c
Azafo Cossa <azafoalexandre@outlook.com>
2025-03-14 21:31:24 +0200

    Add Reservation model, controller, and migration for booking functionality

---
+++ database/migrations/2025_03_14_100115_create_reservations.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Room;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Room::class, 'room_id')->references('id')->on('rooms');
+            $table->foreignIdFor(User::class, 'user_id')->references('id')->on('users');
+            $table->date('check_in');
+            $table->date('check_out');
+            $table->integer('total_price');
+            $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
